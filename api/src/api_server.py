d59a42249558517a4488ec4e535f91d64e9b947f
KeithLin7024 <keithlin724@gmail.com>
2023-10-28 19:15:50 +0800

    update the server use fastAPI

--- api/src/api_server.py
+++ api/src/api_server.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
-import socket
+import uvicorn
+import router
+from base import SERVER_IP, SERVER_PORT, TASK_IMAGE_QUEUE, GenerateImageItem
+from api_task_func import generate_image_queue
+
 
 app = FastAPI()
-SERVER_IP = socket.gethostbyname(socket.gethostname())
-SERVER_PORT = 5000
+
+
+def init_app():
+    app.include_router(router=router.test_router)
+    app.include_router(router=router.ai_router)
+    return
 
 
 @app.get("/")
@@ -11,7 +19,29 @@ async def home():
     return "Hello"
 
 
-if __name__ == "__main__":
-    import uvicorn
+@app.post("/generate")
+async def generate_image_request(generate_image_json: GenerateImageItem):
+    """
+    The function `generate_image_request` enqueues a job to a Redis queue with the specified parameters
+    and returns the task ID.
 
+    :param generate_image_json: The parameter `generate_image_json` is of type `GenerateImageItem`
+    :type generate_image_json: GenerateImageItem
+    :return: a dictionary with a single key-value pair. The key is "task_id" and the value is the ID of
+    the job that was enqueued in the TASK_IMAGE_QUEUE.
+    """
+    job = TASK_IMAGE_QUEUE.enqueue(
+        generate_image_queue,
+        f"http://{SERVER_IP}:{SERVER_PORT}/generate-redis",
+        {
+            "name": generate_image_json.name,
+            "prompt": generate_image_json.prompt,
+        },
+    )
+    return {"task_id": job.get_id()}
+
+
+# https://blog.csdn.net/qq_33801641/article/details/120320780
+if __name__ == "__main__":
+    init_app()
     uvicorn.run(app, host=SERVER_IP, port=SERVER_PORT)
---
+++ api/src/router/__init__.py
@@ -0,0 +1,2 @@
+from .test_router import test_router
+from .ai_route import ai_router
