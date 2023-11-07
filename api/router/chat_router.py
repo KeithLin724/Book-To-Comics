35940d33ca1c644a10fb6c170dc933d40301ac91
KeithLin724 <keithlin724@gmail.com>
2023-11-07 13:23:09 +0800

    update the doc comment use numpy style

--- api/router/chat_router.py
+++ api/router/chat_router.py
@@ -7,16 +7,20 @@ chat_router = APIRouter()
 
 @chat_router.post("/chat")
 async def chat_to_ai(chat_json: message_item.ChatItem):
-    """
-    The function `chat_to_ai` takes a JSON object representing a chat message, generates a reply using a
-    text generation model, and returns the reply as a JSON object.
+    """The function `chat_to_ai` takes a chat message as input, generates a reply using a text generation
+    model, and returns the generated reply.
 
-    :param chat_json: The `chat_json` parameter is a JSON object that contains information about the
-    chat message. It typically includes the message content and any other relevant details such as the
-    sender's name or ID
-    :type chat_json: ChatItem
-    :return: a dictionary with a single key-value pair. The key is "message" and the value is the
-    generated reply message from the AI model.
+    Parameters
+    ----------
+    chat_json : message_item.ChatItem
+        The parameter `chat_json` is of type `message_item.ChatItem`. It represents a chat message in JSON
+    format. It contains the `message` field, which is the text of the chat message.
+
+    Returns
+    -------
+        The function `chat_to_ai` returns a dictionary with a single key-value pair. The key is "message"
+    and the value is the generated reply message from the AI model.
+
     """
     provider, reply_message = await text_generator_model.generate(
         prompt=chat_json.message
--- api_cuda/base/api_json.py
+++ api_cuda/base/api_json.py
@@ -2,6 +2,19 @@ import json
 
 
 def json_to_file(json_obj, file_name: str):
+    """The function `json_to_file` takes a JSON object and a file name as input, and writes the JSON object
+    to a file with the specified name in JSON format.
+
+    Parameters
+    ----------
+    json_obj
+        The `json_obj` parameter is the JSON object that you want to write to a file. It can be any valid
+    JSON object, such as a dictionary, list, or string.
+    file_name : str
+        The `file_name` parameter is a string that represents the name of the file where the JSON object
+    will be saved.
+
+    """
     if not file_name.endswith(".json"):
         file_name = f"{file_name}.json"
 
@@ -10,6 +23,19 @@ def json_to_file(json_obj, file_name: str):
 
 
 def load_json_from_file(file_name: str):
+    """The function `load_json_from_file` loads a JSON object from a file and returns it.
+
+    Parameters
+    ----------
+    file_name : str
+        The `file_name` parameter is a string that represents the name or path of the JSON file that you
+    want to load.
+
+    Returns
+    -------
+        a JSON object.
+
+    """
     with open(file=file_name, mode="r") as f:
         json_object = json.load(f)
     return json_object
