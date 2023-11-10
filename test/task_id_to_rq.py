5d196715797ef86d92caecbc60ea614dd20c01ea
KeithLin724 <keithlin724@gmail.com>
2023-11-10 20:25:11 +0800

    update the test python code

--- test/task_id_to_rq.py
+++ test/task_id_to_rq.py
@@ -1,21 +1,34 @@
 import redis
 from rq.job import Job
+import asyncio
+import time
 
-# 连接到Redis服务器
 redis_conn = redis.Redis(host="140.113.238.35", port=6379)
-job_id = input("id:")
-# 创建RQ队列
-# queue = Queue(connection=redis_conn)
 
-# # 获取任务ID，可以从请求上下文或其他方式获取
-# # 这里使用get_current_job()来获取当前任务的ID，实际中可以根据您的需要获取任务ID
-# job_id = get_current_job().id
 
-# 使用任务ID获取任务对象
-job = Job.fetch(job_id, connection=redis_conn)
+async def get_job_result(job_id: str):
+    job = Job.fetch(job_id, connection=redis_conn)
 
-# 获取任务的执行结果
-result = job.result  # 这将阻塞，直到任务完成
+    def wait_job():
+        while not job.is_finished:
+            # 获取任务的执行结果
+            time.sleep(1)
+        result = job.result  # 这将阻塞，直到任务完成
+        return result
 
-# 输出任务的结果
-print("Task result:", result)
+    return await asyncio.to_thread(wait_job)
+
+
+async def main():
+    # 连接到Redis服务器
+
+    job_id = input("id:")
+    # 创建RQ队列
+    # queue = Queue(connection=redis_conn)
+    result = await get_job_result(job_id=job_id)
+    # 输出任务的结果
+    print("Task result:", result)
+    return
+
+
+asyncio.run(main())
