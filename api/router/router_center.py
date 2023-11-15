8176d6a815225dd6c8f7c5cf265f393a98bef1b0
KeithLin724 <keithlin724@gmail.com>
2023-11-15 16:42:17 +0800

    update uploadfile root

--- api/router/router_center.py
+++ api/router/router_center.py
@@ -27,11 +27,13 @@ import io
 from .chat_router import chat_router
 from .text_to_image_router import text_to_image_router
 from .connect_router import connect_api_router
+from .upload_router import upload_router
 
 router_center = APIRouter()
 router_center.include_router(router=chat_router)
 router_center.include_router(router=text_to_image_router)
 router_center.include_router(router=connect_api_router)
+router_center.include_router(router=upload_router)
 
 
 # @ai_router.post("/chat")
