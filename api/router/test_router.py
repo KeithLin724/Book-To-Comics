c582618dac425b37f51b8382bbb51ad5db11bbca
KeithLin724 <keithlin724@gmail.com>
2023-11-15 16:14:30 +0800

    update two testing root

--- api/router/test_router.py
+++ api/router/test_router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-
+from fastapi.responses import FileResponse
 
 test_router = APIRouter()
 
@@ -19,3 +19,30 @@ async def test_result():
 @test_router.post("/test_result")
 async def test_result_post():
     return {"result": "not ready"}
+
+
+@test_router.get("/test_get_prompt")
+async def test_get_prompt():
+    return {
+        "prompt": [
+            "cat is running",
+            "i am iron man",
+            "dog is running",
+        ],
+    }
+
+
+@test_router.get("/test_get_image")
+async def test_get_image():
+    test_image_path, test_image_name = "./images/cat.jpg", "cat"
+
+    response = FileResponse(
+        test_image_path,
+        media_type="image/jpeg",
+    )
+
+    # response.headers[
+    #     "Content-Disposition"
+    # ] = f'attachment; filename="{test_image_name}.jpg"'
+
+    return response
