815fc4a8739a2f7843e1ac01f43fc55114079ede
KeithLin724 <keithlin724@gmail.com>
2023-11-25 01:39:52 +0800

    update some testing code

--- test/test_load_mode_from_onnx.py
+++ test/test_load_mode_from_onnx.py
@@ -1,7 +1,10 @@
 from optimum.onnxruntime import ORTStableDiffusionPipeline
 
 model_id = "./onnx-stable-diffusion-v1-5"
-pipeline = ORTStableDiffusionPipeline.from_pretrained(model_id)
+pipeline = ORTStableDiffusionPipeline.from_pretrained(
+    model_id,
+    provider="CUDAExecutionProvider",
+)
 prompt = "sailing ship in storm by Leonardo da Vinci"
 image = pipeline(prompt).images[0]
 image.save("sailing ship in storm by Leonardo da Vinci.jpg")
