454878c802879c85a6564b5fbfa0e8d73b0ba0eb
KeithLin724 <keithlin724@gmail.com>
2023-12-11 22:07:33 +0800

    remove the test folder

--- test/old/test_upload_file.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "http://140.113.238.35:5000/uploadfile/"
-files = {
-    "file": ("cat.jpg", open("./cat.jpg", "rb"), "image/jpeg"),
-}
-response = requests.post(url, files=files)
-print(response.json())
